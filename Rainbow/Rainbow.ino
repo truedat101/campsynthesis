9ac9def9cf5fe2df35fd545fdeb88a2690d4bb51
Christian C <30782313+cconaway@users.noreply.github.com>
2019-08-19 11:46:20 -0700

    Add files for Rainbow and Blink LEDs

---
+++ Rainbow/Rainbow.ino
@@ -0,0 +1,60 @@
+#include <OctoWS2811.h>
+
+const int ledsPerStrip = 144;
+
+DMAMEM int displayMemory[ledsPerStrip*6];
+int drawingMemory[ledsPerStrip*6];
+
+const int config = WS2811_GRB | WS2811_800kHz;
+
+OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
+
+int rainbowColors[180];
+
+
+void setup() {
+  pinMode(1, OUTPUT);
+  digitalWrite(1, HIGH);
+  for (int i=0; i<180; i++) {
+    int hue = i * 2;
+    int saturation = 100;
+    int lightness = 50;
+    // pre-compute the 180 rainbow colors
+    rainbowColors[i] = makeColor(hue, saturation, lightness);
+  }
+  digitalWrite(1, LOW);
+  leds.begin();
+}
+
+
+void loop() {
+  rainbow(10, 2500);
+}
+
+// phaseShift is the shift between each row.  phaseShift=0
+// causes all rows to show the same colors moving together.
+// phaseShift=180 causes each row to be the opposite colors
+// as the previous.
+//
+// cycleTime is the number of milliseconds to shift through
+// the entire 360 degrees of the color wheel:
+// Red -> Orange -> Yellow -> Green -> Blue -> Violet -> Red
+//
+void rainbow(int phaseShift, int cycleTime)
+{
+  int color, x, y, wait;
+
+  wait = cycleTime * 1000 / ledsPerStrip;
+  for (color=0; color < 180; color++) {
+    digitalWrite(1, HIGH);
+    for (x=0; x < ledsPerStrip; x++) {
+      for (y=0; y < 8; y++) {
+        int index = (color + x + y*phaseShift/2) % 180;
+        leds.setPixel(x + y*ledsPerStrip, rainbowColors[index]);
+      }
+    }
+    leds.show();
+    digitalWrite(1, LOW);
+    delayMicroseconds(wait);
+  }
+}
---
+++ Testrun1/Testrun1.ino
@@ -0,0 +1,55 @@
+#include <OctoWS2811.h>
+
+const int ledsPerStrip = 144;
+
+DMAMEM int displayMemory[ledsPerStrip*6];
+int drawingMemory[ledsPerStrip*6];
+
+const int config = WS2811_GRB | WS2811_800kHz;
+
+OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
+
+void setup() {
+  leds.begin();
+  leds.show();
+}
+
+#define RED    0xFF0000
+#define GREEN  0x00FF00
+#define BLUE   0x0000FF
+#define YELLOW 0xFFFF00
+#define PINK   0xFF1088
+#define ORANGE 0xE05800
+#define WHITE  0xFFFFFF
+
+// Less intense...
+/*
+#define RED    0x160000
+#define GREEN  0x001600
+#define BLUE   0x000016
+#define YELLOW 0x101400
+#define PINK   0x120009
+#define ORANGE 0x100400
+#define WHITE  0x101010
+*/
+
+void loop() {
+  int microsec = 2000000 / leds.numPixels();  // change them all in 2 seconds
+
+  colorWipe(RED, microsec);
+  colorWipe(GREEN, microsec);
+  colorWipe(BLUE, microsec);
+  colorWipe(YELLOW, microsec);
+  colorWipe(PINK, microsec);
+  colorWipe(ORANGE, microsec);
+  colorWipe(WHITE, microsec);
+}
+
+void colorWipe(int color, int wait)
+{
+  for (int i=0; i < leds.numPixels(); i++) {
+    leds.setPixel(i, color);
+    leds.show();
+    delayMicroseconds(wait);
+  }
+}
